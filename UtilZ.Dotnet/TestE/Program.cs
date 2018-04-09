37c4f3c4fa691837c658f01d47bfcebf91749443
yf <191398892@qq.com>
2018-04-09 20:10:41 +0800

    d

--- UtilZ.Dotnet/TestE/Program.cs
+++ UtilZ.Dotnet/TestE/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using TestE.Common;
+using TestE.DB;
 
 namespace TestE
 {
@@ -16,7 +17,7 @@ namespace TestE
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FTestLMReqResCenter());
+            Application.Run(new FTestPageGrid());
         }
     }
 }
