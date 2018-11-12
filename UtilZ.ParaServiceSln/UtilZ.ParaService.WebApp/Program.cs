b9cd177f17068da467edc2ec765928b2db8bb5da
yf <191398892@qq.com>
2018-11-12 20:59:57 +0800

    添加监听配置

--- UtilZ.ParaServiceSln/UtilZ.ParaService.WebApp/Program.cs
+++ UtilZ.ParaServiceSln/UtilZ.ParaService.WebApp/Program.cs
@@ -14,14 +14,19 @@ namespace UtilZ.ParaService.WebApp
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("hosting.json", optional: true)
+            .Build();
+            CreateWebHostBuilder(args, config).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args, IConfiguration config) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())//定义contentroot  
                 .UseWebRoot(Directory.GetCurrentDirectory() + "/wwwroot")//定义webroot
-                .UseUrls("http://*:12018", "https://*:22018")
+                //.UseUrls("http://*:12018", "https://*:22018")
                 .UseStartup<Startup>();
     }
 }
