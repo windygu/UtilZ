a1e0b2736cd4e454d8363b13ca49da2aac471f84
yf <191398892@qq.com>
2018-11-16 21:39:16 +0800

    v

---
+++ UtilZ.ParaServiceSln/UtilZ.ParaService.WebApp/Models/ParaServiceConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UtilZ.ParaService.WebApp.Models
+{
+    public class ParaServiceConstant
+    {
+        public const string Secret = "JwtBearerSample_11231~#$%#%^2235";
+    }
+}
