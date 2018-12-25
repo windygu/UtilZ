04e90201cbb8fda6121a96173b1cc4602b161588
yf <191398892@qq.com>
2018-12-25 19:52:01 +0800

    y

--- UtilZ.ParaServiceSln/UtilZ.ParaService.WebApp/Controllers/1_0/ActionAuthenticationFilter.cs
+++ UtilZ.ParaServiceSln/UtilZ.ParaService.WebApp/Controllers/1_0/ActionAuthenticationFilter.cs
@@ -11,49 +11,122 @@ namespace UtilZ.ParaService.WebApp.Controllers._1_0
 {
     public class ActionAuthenticationFilter : IActionFilter
     {
+        /*
         public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!this.IsFilter(context))
+            {
+                return;
+            }
+
+            var controller = (ControllerBase)context.Controller;
+            var token = controller.Request.Headers[WebAppConstant.AccessToken];
+            var userInfo = AuthenticationController.GetUserInfo(token);
+            if (userInfo == null)
+            {
+                context.Result = controller.Unauthorized();
+                context.Canceled = true;
+            }
+
+            if (!context.ModelState.IsValid)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in context.ModelState.Values)
+                {
+                    foreach (var error in item.Errors)
+                    {
+                        sb.Append(error.ErrorMessage);
+                        sb.Append("|");
+                    }
+                }
+
+                if (sb.Length > 0)
+                {
+                    sb = sb.Remove(sb.Length - 1, 1);
+                }
+
+                context.Result = new ActionResult<string>(sb.ToString()).Result;
+                context.Canceled = true;
+            }
+        }
+
+        private bool IsFilter(ActionExecutedContext context)
         {
             var controller = (ControllerBase)context.Controller;
             if (controller is AuthenticationController)
             {
                 //登录，不作验证
+                return false;
             }
-            else
+
+            if (controller is ParaValueController && string.Equals(context.ActionDescriptor.RouteValues["action"], "get", StringComparison.OrdinalIgnoreCase))
             {
-                var token = controller.Request.Headers[WebAppConstant.AccessToken];
-                var userInfo = AuthenticationController.GetUserInfo(token);
-                if (userInfo == null)
+                return false;
+            }
+
+            return true;
+        }*/
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in context.ModelState.Values)
                 {
-                    context.Result = controller.Unauthorized();
-                    context.Canceled = true;
+                    foreach (var error in item.Errors)
+                    {
+                        sb.Append(error.ErrorMessage);
+                        sb.Append("|");
+                    }
                 }
 
-                if (!context.ModelState.IsValid)
+                if (sb.Length > 0)
                 {
-                    StringBuilder sb = new StringBuilder();
-                    foreach (var item in context.ModelState.Values)
-                    {
-                        foreach (var error in item.Errors)
-                        {
-                            sb.Append(error.ErrorMessage);
-                            sb.Append("|");
-                        }
-                    }
-
-                    if (sb.Length > 0)
-                    {
-                        sb = sb.Remove(sb.Length - 1, 1);
-                    }
-
-                    context.Result = new ActionResult<string>(sb.ToString()).Result;
-                    context.Canceled = true;
+                    sb = sb.Remove(sb.Length - 1, 1);
                 }
+
+                context.Result = new ActionResult<string>(sb.ToString()).Result;
+                context.Canceled = true;
             }
         }
 
         public void OnActionExecuting(ActionExecutingContext context)
         {
+            if (!this.IsFilter(context))
+            {
+                return;
+            }
 
+            var controller = (ControllerBase)context.Controller;
+            var token = controller.Request.Headers[WebAppConstant.AccessToken];
+            var userInfo = AuthenticationController.GetUserInfo(token);
+            if (userInfo == null)
+            {
+                context.Result = controller.Unauthorized();
+            }
+
+
+            //var controller = (ControllerBase)context.Controller;
+            //context.Result = controller.Unauthorized();
+            //context.Result = new EmptyResult();
+        }
+
+        private bool IsFilter(ActionExecutingContext context)
+        {
+            var controller = (ControllerBase)context.Controller;
+            if (controller is AuthenticationController)
+            {
+                //登录，不作验证
+                return false;
+            }
+
+            if (controller is ParaValueController && string.Equals(context.ActionDescriptor.RouteValues["action"], "get", StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
